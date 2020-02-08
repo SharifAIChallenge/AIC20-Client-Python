23e860b792e42a39f7034a49c8af73936b0bc9fd
ErfanFaravani <erfanfaravani@gmail.com>
2020-02-08 18:59:24 +0330

    name fixed

--- AI.py
+++ AI.py
@@ -1,10 +1,9 @@
 class AI:
-
+    # this function is called in the beginning for deck picking and preprocess
     def pick(self, world):
         print("pick")
-        #self.path_to_friend_check(world)
+        # self.path_to_friend_check(world)
         world.choose_deck([1, 2, 3, 4])
-
 
     def path_to_friend_check(self, world):
         # path check:
@@ -20,6 +19,7 @@ class AI:
         print(world.get_second_enemy())
         print(world.get_second_enemy().path_to_friend)
 
+    # it is called every turn for doing process during the game
     def turn(self, world):
         print("turn")
         if world.get_current_turn() == 26:
@@ -35,6 +35,6 @@ class AI:
             print("its turn 2!!!")
             world.put_unit(base_unit=world.get_me().hand[0], path=world.get_me().paths_from_player[0])
 
+    #
     def end(self, world, scores):
         print("ending")
-
