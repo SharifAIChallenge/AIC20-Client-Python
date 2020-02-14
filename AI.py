8b66825aaf5ec323d75ec51c767dfaddde62060c
sina <aaa>
2020-02-15 00:25:18 +0330

    Fix str bug

--- AI.py
+++ AI.py
@@ -30,7 +30,7 @@ class AI:
 
     # it is called every turn for doing process during the game
     def turn(self, world):
-        print("turn started: " + world.get_current_turn())
+        print("turn started: " + str(world.get_current_turn()))
 
         myself = world.get_me()
         max_ap = world.game_constants.max_ap
