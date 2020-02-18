a385990f6f4073912921e243910f5b3ebc1c51b9
ErfanFaravani <erfanfaravani@gmail.com>
2020-02-18 22:07:15 +0330

    Fixing Get_spell_count in Player Class

--- AI.py
+++ AI.py
@@ -32,7 +32,6 @@ class AI:
     # it is called every turn for doing process during the game
     def turn(self, world):
         print("turn started:", world.get_current_turn())
-
         myself = world.get_me()
         max_ap = world.get_game_constants().max_ap
 
