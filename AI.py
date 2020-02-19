67866769f5be1eb5dc5d8550a559ec689d45a16b
hamidrezakamkari <hamidrezakamkari@gmail.com>
2020-02-19 23:06:10 +0330

    typing added!

--- AI.py
+++ AI.py
@@ -2,6 +2,7 @@ import random
 
 from model import *
 
+from world import World
 
 class AI:
     def __init__(self):
@@ -10,10 +11,8 @@ class AI:
         self.path_for_my_units = None
 
     # this function is called in the beginning for deck picking and pre process
-    def pick(self, world):
+    def pick(self, world:World):
         print("pick started!")
-
-        world.get_cast_spell_by_id(id=1.1)
 
         # pre process
         map = world.get_map()
@@ -30,7 +29,7 @@ class AI:
         self.path_for_my_units = world.get_friend().paths_from_player[0]
 
     # it is called every turn for doing process during the game
-    def turn(self, world):
+    def turn(self, world:World):
         print("turn started:", world.get_current_turn())
         myself = world.get_me()
         max_ap = world.get_game_constants().max_ap
@@ -75,6 +74,6 @@ class AI:
     # it is called after the game ended and it does not affect the game.
     # using this function you can access the result of the game.
     # scores is a map from int to int which the key is player_id and value is player_score
-    def end(self, world, scores):
+    def end(self, world:World, scores):
         print("end started!")
         print("My score:", scores[world.get_me().player_id])
